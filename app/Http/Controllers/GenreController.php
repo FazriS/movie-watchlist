097926678e1c8c11c38855a5267920b8d2eef642
Archie <57532087+Rockblank@users.noreply.github.com>
2026-03-31 00:21:06 +0700

    GenreController - Adding new function (ShowAll, Create, ShowByID)

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Genre;
 use Illuminate\Http\Request;
 
 class GenreController extends Controller
 {
-    //
+    public function createGenre(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:255'
+        ]);
+
+        $genre = Genre::create([
+            'name' => $request->name
+        ])
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Genre created successfully',
+            'genre' => $genre
+        ]);
+    }
+
+    public function getAllGenres(){
+        $genres = Genre::all();
+        return response()->json([
+            'success' => true,
+            'message' => 'Genres retrieved successfully',
+            'genres' => $genres
+        ]);
+    }
+
+    public function getGenreById($id){
+        $genres = Genre::findOrFail($id);
+        return response()->json([
+            'success' => true,
+            'message' => 'Showing genre with id ' . $id,
+            'genres' => $genres
+        ]);
+    }
 }
